dfbdbec3409b0ca3e7b2d4df47352623d33b5d40
Shakira <shakira.r.salazar@gmail.com>
2021-10-30 21:37:54 +0100

    fixed fetch request error

--- src/apiCall.js
+++ src/apiCall.js
@@ -19,8 +19,8 @@ function renderResponse(responseData){
 let cityInput = document.getElementById('cityName');
 cityInput.addEventListener("change", (e) =>{
 	const cityName = e.target.value;
-	const weatherAPIKey = 2c4642d2d77ebdccb8ca6984c1a349b2
-	const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${weatherAPIKey}`
+	const weatherAPIKey = '2c4642d2d77ebdccb8ca6984c1a349b2'
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${weatherAPIKey}`
 
 	getWeatherData(url).then(response =>{
 		let results = renderResponse(response);
